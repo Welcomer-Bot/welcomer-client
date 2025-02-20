1ddac91203221f629486975b5c00fbcc00ddeea9
Clement <contact@welcomer.app>
2025-02-20 23:40:18 +0100

    remove unused import

--- app/auth/callback/page.tsx
+++ app/auth/callback/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button, Card, CardBody } from "@heroui/react";
-import { redirect, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { use, useEffect } from "react";
 
 export default function Page({
@@ -9,32 +9,31 @@ export default function Page({
 }: {
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
- const { code, error, error_description } = use(searchParams);
- const router = useRouter();
+  const { code, error, error_description } = use(searchParams);
+  const router = useRouter();
 
- useEffect(() => {
-   if (error) {
-     if (error == "access_denied") {
-       router.push("/");
-       return;
-     }
-     router.push(
-       `/auth/error?error=${error}&error_description=${error_description}`
-     );
-     return;
-   }
-     fetch(`/api/auth/callback?code=${code}`)
-         .then((res) => res.json())
-         .then((data) => {
-            if (data.error) {
-              return router.push(
-                 `/auth/error?error=${data.error}&error_description=${data.error_description}`
-              );
-            }
-             return router.push("/dashboard");
-            });
-        
- }, [code, error, error_description, router]);
+  useEffect(() => {
+    if (error) {
+      if (error == "access_denied") {
+        router.push("/");
+        return;
+      }
+      router.push(
+        `/auth/error?error=${error}&error_description=${error_description}`
+      );
+      return;
+    }
+    fetch(`/api/auth/callback?code=${code}`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.error) {
+          return router.push(
+            `/auth/error?error=${data.error}&error_description=${data.error_description}`
+          );
+        }
+        return router.push("/dashboard");
+      });
+  }, [code, error, error_description, router]);
 
   return (
     <section className="flex flex-col h-full items-center justify-center gap-4 py-8 md:py-10">
