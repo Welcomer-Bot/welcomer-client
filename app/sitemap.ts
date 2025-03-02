33084a68ee034203c0ecb1798449b6d73d7982e8
Clement <contact@welcomer.app>
2025-03-02 23:25:05 +0100

    update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,19 +3,19 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: 'beta.welcomer.app',
+            url: 'https://beta.welcomer.app',
             lastModified: new Date(),
             changeFrequency: 'yearly',
             priority: 1,
         },
         {
-            url: 'beta.welcomer.app/dashboard',
+            url: 'https://beta.welcomer.app/dashboard',
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 0.8,
         },
         {
-            url: 'beta.welcomer.app/support',
+            url: 'https://beta.welcomer.app/support',
             lastModified: new Date(),
             changeFrequency: 'weekly',
             priority: 0.5,
