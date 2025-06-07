de720de86aca5891c5378dd6bd983e517e5539b4
Clement <contact@welcomer.app>
2025-06-07 11:03:37 +0200

    fix: update InviteBotButton to use anchor tag for authorization link

--- components/dashboard/InviteBotButton.tsx
+++ components/dashboard/InviteBotButton.tsx
@@ -1,27 +1,29 @@
 "use client";
 import { Button } from "@heroui/react";
 import { usePlausible } from "next-plausible";
-import { redirect } from "next/navigation";
 import { useState } from "react";
 const DISCORD_CLIENT_ID = process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID!;
 
 export default function InviteBotButton({ guildId }: { guildId?: string }) {
   const [loading, setLoading] = useState(false);
   const plausible = usePlausible();
+  console.log("DISCORD_CLIENT_ID", DISCORD_CLIENT_ID);
 
   return (
-    <Button
-      color="primary"
-      isLoading={loading}
-      onPress={async () => {
-        setLoading(true);
-        plausible("dashboard-invite-bot");
-        redirect(
-          `https://discord.com/api/oauth2/authorize?client_id=${DISCORD_CLIENT_ID}&permissions=8&scope=bot&guild_id=${guildId}`
-        );
-      }}
+    <a
+      href={`https://discord.com/api/oauth2/authorize?client_id=${DISCORD_CLIENT_ID}
+    &permissions=8&scope=bot&guild_id=${guildId}`}
     >
-      Invite Bot
-    </Button>
+      <Button
+        color="primary"
+        isLoading={loading}
+        onPress={async () => {
+          setLoading(true);
+          plausible("dashboard-invite-bot");
+        }}
+      >
+        Invite Bot
+      </Button>
+    </a>
   );
 }
