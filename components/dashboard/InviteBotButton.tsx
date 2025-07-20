185f9abddd499b9f1ca5890cf9d313f632504cc8
Clement <contact@welcomer.app>
2025-07-20 12:37:13 +0200

    fix: refactor InviteBotButton to load client id at the first load

--- components/dashboard/InviteBotButton.tsx
+++ components/dashboard/InviteBotButton.tsx
@@ -1,13 +1,20 @@
 "use client";
 import { Button } from "@heroui/react";
 import { usePlausible } from "next-plausible";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 const DISCORD_CLIENT_ID = process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID!;
 
 export default function InviteBotButton({ guildId }: { guildId?: string }) {
   const [loading, setLoading] = useState(false);
+  const [clientId, setClientId] = useState(DISCORD_CLIENT_ID);
   const plausible = usePlausible();
-  console.log("DISCORD_CLIENT_ID", DISCORD_CLIENT_ID);
+  useEffect(() => {
+    if (!clientId) {
+      console.log("DISCORD_CLIENT_ID", DISCORD_CLIENT_ID);
+      setClientId(DISCORD_CLIENT_ID);
+    }
+  }, [clientId]);
+    
 
   return (
     <a
