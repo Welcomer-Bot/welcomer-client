99aa7a9415f2b8d9b1b4a16cb538f6e01908a506
Clement <contact@welcomer.app>
2025-07-27 17:46:53 +0200

    refactor: update InviteBotButton component to use Link and simplify client ID handling; remove unused tailwind.config.js, update tailwind & heroui  version

--- components/dashboard/InviteBotButton.tsx
+++ components/dashboard/InviteBotButton.tsx
@@ -1,25 +1,17 @@
 "use client";
 import { Button } from "@heroui/react";
 import { usePlausible } from "next-plausible";
-import { useState, useEffect } from "react";
+import Link from "next/link";
+import { useState } from "react";
 const DISCORD_CLIENT_ID = process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID!;
 
 export default function InviteBotButton({ guildId }: { guildId?: string }) {
   const [loading, setLoading] = useState(false);
-  const [clientId, setClientId] = useState(DISCORD_CLIENT_ID);
   const plausible = usePlausible();
-  useEffect(() => {
-    if (!clientId) {
-      console.log("DISCORD_CLIENT_ID", DISCORD_CLIENT_ID);
-      setClientId(DISCORD_CLIENT_ID);
-    }
-  }, [clientId]);
-    
 
   return (
-    <a
-      href={`https://discord.com/api/oauth2/authorize?client_id=${clientId}
-    &permissions=8&scope=bot&guild_id=${guildId}`}
+    <Link
+      href={`https://discord.com/api/oauth2/authorize?client_id=${DISCORD_CLIENT_ID}&permissions=8&scope=bot%20applications.commands&guild_id=${guildId}`}
     >
       <Button
         color="primary"
@@ -29,8 +21,8 @@ export default function InviteBotButton({ guildId }: { guildId?: string }) {
           plausible("dashboard-invite-bot");
         }}
       >
-        Invite Bot
+        Invite bot
       </Button>
-    </a>
+    </Link>
   );
 }
